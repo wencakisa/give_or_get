505cbb7ff1ddb4d02263a4c7f5d1cd3b66889e89
wencakisa <wencakisa@gmail.com>
2017-07-11 14:00:30 +0300

    Added filter for item owner

--- items/filters.py
+++ items/filters.py
@@ -1,7 +1,12 @@
 from rest_framework import filters
 
 
-class IsActiveFilterBackend(filters.BaseFilterBackend):
+class IsItemOwnerFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(owner=request.user)
+
+
+class IsActiveItemFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         is_active = request.query_params.get('is_active', 'true')
         is_active = is_active.lower() == 'true'
